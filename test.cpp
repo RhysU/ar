54b18485ba0eb89dc8e162fbaacbaf8ec8ac7858
Rhys Ulerich <rhys.ulerich@gmail.com>
2012-07-22 23:49:57 -0500

    Add YW solve as consistency check

--- test.cpp
+++ test.cpp
@@ -5,6 +5,8 @@
 #include "burg.hpp"
 
 #include <algorithm>
+#include <cmath>
+#include <functional>
 #include <cstdio>
 #include <iostream>
 #include <iterator>
@@ -14,45 +16,61 @@
 // Computes percent difference of \c b against theoretical result \c a.
 template<typename FPT> FPT pdiff(FPT a, FPT b) { return (b - a) / a * 100; }
 
+template<typename T> struct sum_error : public std::binary_function<T,T,T> {
+    T operator() (T a, T b) {using std::abs; return a + abs(b);}
+};
+
+// Set working precision
+typedef double real;
+
 // Test burg_algorithm against synthetic data
 int main(int argc, char *argv[])
 {
     using namespace std;
 
-    vector<double> exact;
+    vector<real> exact;
     {
         ifstream f;
         f.exceptions(ifstream::badbit);
         f.open(argc > 1 ? argv[1] : "test1.coeff");
-        copy(istream_iterator<double>(f), istream_iterator<double>(),
+        copy(istream_iterator<real>(f), istream_iterator<real>(),
              back_inserter(exact));
     }
-    vector<double> est(exact.size()), est_rho(exact.size());
+    vector<real> est(exact.size()), cor(exact.size());
 
-    vector<double> data;
+    vector<real> data;
     {
         ifstream f;
         f.exceptions(ifstream::badbit);
         f.open(argc > 2 ? argv[2] : "test1.dat");
-        copy(istream_iterator<double>(f), istream_iterator<double>(),
+        copy(istream_iterator<real>(f), istream_iterator<real>(),
              back_inserter(data));
     }
 
-    // Compute burg_algorithm's answer
-    double est_sigma2e, est_gain;
+    // Use burg_algorithm to fit an AR model and characterize it completely
+    real sigma2e, gain;
     burg_algorithm(data.begin(), data.end(), est.begin(), est.end(),
-                   &est_sigma2e, &est_gain, est_rho.begin());
+                   &sigma2e, &gain, cor.begin());
+
+    // Solve Yule-Walker equations using Zohar's algorithm as consistency check
+    // Given right hand side containing rho_1, ..., rho_p the solution should
+    // be -a_1, ..., -a_p on success so adding to it a_1, ..., a_p gives errors.
+    vector<real> rhs(cor);
+    zohar_linear_solve(cor.begin(), --cor.end(), cor.begin(), rhs.begin());
+    transform(rhs.begin(), rhs.end(), est.begin(), rhs.begin(), plus<real>());
+    real res = accumulate(rhs.begin(), rhs.end(), real(0), sum_error<real>());
 
     printf("%22s %22s %22s\n", "Coefficient", "burg_algorithm", "PercentDiff");
     printf("%22s %22s %22s\n", "-----------", "--------------", "-----------");
-    for (vector<double>::const_iterator i = exact.begin(), j = est.begin();
+    for (vector<real>::const_iterator i = exact.begin(), j = est.begin();
          i != exact.end(); ++i, ++j)
     {
         printf("%22.14g %22.14g %22.14g\n", *i, *j, pdiff(*i, *j));
     }
     printf("\n");
-    printf("%22s %22.14g\n", "\\sigma^2_\\epsilon", est_sigma2e);
-    printf("%22s %22.14g\n", "gain",                est_gain);
+    printf("%22s %22.14g\n", "Yule-Walker residual:", res);
+    printf("%22s %22.14g\n", "\\sigma^2_\\epsilon:",  sigma2e);
+    printf("%22s %22.14g\n", "signal gain:",          gain);
 
     return 0;
 }
