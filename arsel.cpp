8823a987f7dd18cc67d167bcd49d17e202029d9b
Rhys Ulerich <rhys.ulerich@gmail.com>
2012-07-27 14:01:43 -0500

    use the new best_model method to simplify code

--- arsel.cpp
+++ arsel.cpp
@@ -16,56 +16,7 @@
 #include <limits>
 #include <vector>
 
-template <class Real, class In>
-std::size_t process(In& in,
-                    Real& mean,
-                    std::size_t &order,
-                    std::vector<Real>& params,
-                    std::vector<Real>& sigma2e,
-                    std::vector<Real>& gain,
-                    std::vector<Real>& autocor,
-                    bool subtract_mean)
-{
-    // Use burg_method to estimate a hierarchy of AR models from input data
-    params .reserve(order*(order + 1)/2);
-    sigma2e.reserve(order + 1);
-    gain   .reserve(order + 1);
-    autocor.reserve(order + 1);
-    const std::size_t N = burg_method(std::istream_iterator<Real>(in),
-                                      std::istream_iterator<Real>(),
-                                      mean,
-                                      order,
-                                      std::back_inserter(params),
-                                      std::back_inserter(sigma2e),
-                                      std::back_inserter(gain),
-                                      std::back_inserter(autocor),
-                                      subtract_mean,
-                                      /* output hierarchy? */ true);
-
-    // Find the best model according to CIC accounting for subtract_mean.
-    typename std::vector<Real>::difference_type best;
-    if (subtract_mean) {
-        best = evaluate_models<CIC<Burg<mean_subtracted> > >(
-                    N, 0u, sigma2e.begin(), sigma2e.end());
-    } else {
-        best = evaluate_models<CIC<Burg<mean_retained> > >(
-                    N, 0u, sigma2e.begin(), sigma2e.end());
-    }
-
-    // Trim away everything but the best model (where ranges might overlap)
-    // AR(0) is trivial and AR(1) starts at params.begin(), hence off by one.
-    std::copy_backward(params.begin() + ((best-1)*best)/2,
-                       params.begin() + ((best-1)*best)/2 + best,
-                       params.begin() + best);
-    params.resize(best);
-    sigma2e[0] = sigma2e[best]; sigma2e.resize(1);
-    gain   [0] = gain   [best]; gain   .resize(1);
-    autocor.resize(best + 1);
-
-    return N;
-}
-
-// Test burg_method against synthetic data
+// Estimate the best AR(p) model given data on standard input
 int main(int argc, char *argv[])
 {
     using namespace std;
@@ -79,12 +30,35 @@ int main(int argc, char *argv[])
         --argc;
     }
 
+    // Use burg_method to estimate a hierarchy of AR models from input data
     double mean;
     size_t order = 512;
     vector<double> params, sigma2e, gain, autocor;
-    size_t N = process(cin, mean, order, params,
-                       sigma2e, gain, autocor, subtract_mean);
+    params .reserve(order*(order + 1)/2);
+    sigma2e.reserve(order + 1);
+    gain   .reserve(order + 1);
+    autocor.reserve(order + 1);
+    const std::size_t N = burg_method(std::istream_iterator<double>(cin),
+                                      std::istream_iterator<double>(),
+                                      mean,
+                                      order,
+                                      std::back_inserter(params),
+                                      std::back_inserter(sigma2e),
+                                      std::back_inserter(gain),
+                                      std::back_inserter(autocor),
+                                      subtract_mean,
+                                      /* output hierarchy? */ true);
 
+    // Keep only best model according to CIC accounting for subtract_mean.
+    if (subtract_mean) {
+        best_model<CIC<Burg<mean_subtracted> > >(
+                N, params, sigma2e, gain, autocor);
+    } else {
+        best_model<CIC<Burg<mean_retained> > >(
+                N, params, sigma2e, gain, autocor);
+    }
+
+    // Output some details about the best model
     cout.precision(std::numeric_limits<double>::digits10 + 2);
     cout << showpos
          <<   "# N                   "   << N
