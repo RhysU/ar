1d68eda388af7a9ca662a06484eb167348cd575c
Rhys Ulerich <rhys.ulerich@gmail.com>
2012-07-31 14:46:05 -0500

    Also output effective sample size

--- arsel-octfile.cpp
+++ arsel-octfile.cpp
@@ -17,6 +17,7 @@
 
 #include <algorithm>
 #include <cstdlib>
+#include <limits>
 #include <iterator>
 #include <vector>
 
@@ -27,34 +28,43 @@
  * Compare \ref arsel.cpp.
  */
 
+// Compile-time defaults in the code also appearing in the help message
+#define DEFAULT_SUBMEAN  true
+#define DEFAULT_MAXORDER 512
+#define STRINGIFY(x) STRINGIFY_HELPER(x)
+#define STRINGIFY_HELPER(x) #x
+
 DEFUN_DLD(
     arsel, args, nargout,
-    "\t[A, mu, sigma2eps, eff_sigma2x, T0] = arsel (d, submean=true, order=512)\n"
+    "\t[A, mu, sigma2eps, eff_sigma2x, Neff, T0] = arsel (d, submean, maxorder)\n"
     "\tAutomatically fit an autoregressive model to an input signal.\n"
     "\t\n"
     "\tUse ar::burg_method followed by ar::best_model<CIC<Burg<MeanHandling > >\n"
     "\tto obtain the most likely autoregressive process for input signal d.\n"
     "\tThe sample mean of d will be subtracted whenever submean is true.\n"
-    "\tModel orders zero through min(size(d), order) will be considered.\n"
+    "\tModel orders zero through min(size(d), maxorder) will be considered.\n"
     "\t\n"
     "\tThe filter()-ready process parameters are returned in A, the sample mean\n"
     "\tin mu, and the innovation variance \\sigma^2_\\epsilon in sigma2eps.\n"
     "\tGiven the observed autocorrelation structure, a decorrelation time T0 is\n"
     "\tcomputed and used to estimate the effective signal variance eff_sigma2x.\n"
+    "\tThe number of effectively independent samples is returned in Neff.\n"
     "\t\n"
     "\tOne may simulate N samples from the fitted process by afterwards calling:\n"
     "\t\n"
     "\t\tx = mu + filter([1], A, sqrt(sigma2eps)*randn(N,1));\n"
     "\t\n"
+    "\tWhen not supplied, submean defaults to " STRINGIFY(DEFAULT_SUBMEAN) ".\n"
+    "\tWhen not supplied, maxorder defaults to " STRINGIFY(DEFAULT_MAXORDER) ".\n"
 )
 {
-    std::size_t order = 512;
-    bool        submean = true;
+    std::size_t maxorder = DEFAULT_MAXORDER;
+    bool        submean  = DEFAULT_SUBMEAN;
     RowVector   data;
     switch (args.length())  // Cases deliberately fall through
     {
         case 3:
-            order = args(2).ulong_value();
+            maxorder = args(2).ulong_value();
         case 2:
             submean = args(1).bool_value();
         case 1:
@@ -77,12 +87,12 @@ DEFUN_DLD(
     // Use burg_method to estimate a hierarchy of AR models from input data
     double mu;
     std::vector<double> params, sigma2e, gain, autocor;
-    params .reserve(order*(order + 1)/2);
-    sigma2e.reserve(order + 1);
-    gain   .reserve(order + 1);
-    autocor.reserve(order + 1);
+    params .reserve(maxorder*(maxorder + 1)/2);
+    sigma2e.reserve(maxorder + 1);
+    gain   .reserve(maxorder + 1);
+    autocor.reserve(maxorder + 1);
     ar::burg_method(data.fortran_vec(), data.fortran_vec() + N,
-                    mu, order,
+                    mu, maxorder,
                     std::back_inserter(params),
                     std::back_inserter(sigma2e),
                     std::back_inserter(gain),
@@ -100,9 +110,12 @@ DEFUN_DLD(
     }
 
     // Compute decorrelation time from the estimated autocorrelation model
-    const double T0  = ar::decorrelation_time(N, ar::autocorrelation(
-                params.begin(), params.end(), gain[0], autocor.begin()),
-                /* abs(rho) */ true);
+    double T0 = std::numeric_limits<double>::quiet_NaN();
+    if (nargout > 2) {
+        ar::predictor<double> p = ar::autocorrelation(
+                params.begin(), params.end(), gain[0], autocor.begin());
+        T0 = ar::decorrelation_time(N, p, /* abs(rho) */ true);
+    }
 
     // Prepare autoregressive process parameters for Octave's filter function
     RowVector A(params.size() + 1, /* leading one */ 1);
@@ -114,8 +127,10 @@ DEFUN_DLD(
     {
         default:
             warning("arsel: too many output values requested");
-        case 5:
+        case 6:
             retval.prepend(T0);
+        case 5:
+            retval.prepend(N / T0);
         case 4:
             retval.prepend((N*gain[0]*sigma2e[0]) / (N - T0));
         case 3:
