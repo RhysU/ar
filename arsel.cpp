a816cda01b9fc9d513ca3128969be8112145f897
Rhys Ulerich <rhys.ulerich@gmail.com>
2012-10-04 00:14:23 -0500

    Add runtime criterion selection to arsel

--- arsel.cpp
+++ arsel.cpp
@@ -26,54 +26,90 @@
 #include <limits>
 #include <vector>
 
+// Forward declarations for argument checking logic
+struct Arg : public option::Arg
+{
+    static option::ArgStatus NonEmpty(const option::Option& opt, bool msg);
+    static option::ArgStatus Numeric (const option::Option& opt, bool msg);
+};
+
 // Command line argument declarations for optionparser.h usage
-enum OptionIndex { UNKNOWN, HELP, SUBMEAN, SIGNRHO };
+enum OptionIndex { UNKNOWN, CRITERION, HELP, SUBMEAN, SIGNRHO };
 const option::Descriptor usage[] = {
     {UNKNOWN, 0, "", "",      option::Arg::None,
      "Usage: arsel [OPTION]...\n"
      "Fit an optimal autoregressive model to data from standard input.\n"
      "\n"
      "Options:" },
-    {0,0,"","",option::Arg::None,0}, // table break
-    {SUBMEAN, 0,  "",  "subtract-mean", option::Arg::None,
-     "\t--subtract-mean  \tSubtract the sample mean from the incoming data" },
-    {SIGNRHO, 0,  "",  "signed-rho",    option::Arg::None,
-     "\t--signed-rho     \tUse signed autocorrelation value when computing T0" },
-    {HELP,    0,  "h", "help",        option::Arg::None,
+    {0,0,"","",Arg::None,0}, // table break
+    {CRITERION, 0,  "c",  "criterion",     Arg::NonEmpty,
+     "  -c \t--criterion=ABBREV  \tUse the specified model order selection criterion" },
+    {HELP,      0,  "h", "help",           Arg::None,
      "  -h \t--help        \tDisplay this help message and immediately exit" },
+    {SUBMEAN,   0,  "m",  "subtract-mean", Arg::None,
+     "  -m \t--subtract-mean  \tSubtract the sample mean from the incoming data" },
+    {SIGNRHO,   0,  "r",  "signed-rho",    Arg::None,
+     "  -r \t--signed-rho     \tUse signed autocorrelation values when computing T0" },
     {0,0,0,0,0,0}
 };
 
 int main(int argc, char *argv[])
 {
-    using namespace ar;
     using namespace std;
-    using option::Option;
 
     // TODO Add processing of files specified on the command line
 
-    // Process any incoming command line arguments using optionparser.h
-    argc-=(argc>0); argv+=(argc>0); // Skip program name argv[0] if present
-    option::Stats stats(usage, argc, argv);
-    struct Guard { // scoped_array or unique_ptr would simply RAII here
-        Option *options, *buffer;
-        Guard(Option *options, Option *buffer)
-            : options(options), buffer(buffer) {}
-        ~Guard() { delete[] options; delete[] buffer; }
-    } guard(new Option[stats.options_max], new Option[stats.buffer_max ]);
-    option::Parser parse(usage, argc, argv, guard.options, guard.buffer);
-    if (parse.error() || guard.options[UNKNOWN]) {
-        for (Option* o = guard.options[UNKNOWN]; o; o = o->next()) {
-            cerr << "Unknown option: " << o->name << "\n";
+    // Parse and process any command line arguments using optionparser.h
+    string criterion   = "";
+    bool subtract_mean = false;
+    bool absolute_rho  = true;
+    {
+        using namespace option;
+
+        Stats stats(usage, argc-(argc>0), argv+(argc>0));
+
+        // scoped_array or unique_ptr would simply RAII but add dependencies
+        struct Guard {
+            Option *options, *buffer;
+            Guard(Option *options, Option *buffer)
+                : options(options), buffer(buffer) {}
+            ~Guard() { delete[] options; delete[] buffer; }
+        } g(new Option[stats.options_max], new Option[stats.buffer_max]);
+
+        Parser parse(usage, argc-(argc>0), argv+(argc>0), g.options, g.buffer);
+
+        if (parse.error() || g.options[UNKNOWN]) {
+            for (Option* o = g.options[UNKNOWN]; o; o = o->next()) {
+                cerr << "Unknown option: " << o->name << "\n";
+            }
+            return EXIT_FAILURE;
         }
+
+        if (g.options[HELP]) {
+            printUsage(cout, usage);
+            return EXIT_SUCCESS;
+        }
+
+        if (g.options[CRITERION])
+            criterion = g.options[CRITERION].last()->arg;
+
+        if (g.options[SUBMEAN])
+            subtract_mean = true;
+
+        if (g.options[SIGNRHO])
+            absolute_rho = false;
+    }
+
+    // Look up desired model selection criterion using ar::best_model_function
+    // best_model_function template parameters fit ar::burg_method usage below
+    typedef ar::best_model_function<
+            ar::Burg,size_t,vector<double> > best_model_function;
+    const best_model_function::type best_model
+            = best_model_function::lookup(criterion, subtract_mean);
+    if (!best_model) {
+        cerr << "Unknown model selection criterion: " << criterion << "\n";
         return EXIT_FAILURE;
     }
-    if (guard.options[HELP]) {
-        option::printUsage(std::cout, usage);
-        return EXIT_SUCCESS;
-    }
-    const bool subtract_mean = guard.options[SUBMEAN] ? true : false;
-    const bool absolute_rho  = guard.options[SIGNRHO] ? false : true;
 
     // Use burg_method to estimate a hierarchy of AR models from input data
     double mean;
@@ -83,29 +119,25 @@ int main(int argc, char *argv[])
     sigma2e.reserve(order + 1);
     gain   .reserve(order + 1);
     autocor.reserve(order + 1);
-    const size_t N = burg_method(istream_iterator<double>(cin),
-                                 istream_iterator<double>(),
-                                 mean,
-                                 order,
-                                 back_inserter(params),
-                                 back_inserter(sigma2e),
-                                 back_inserter(gain),
-                                 back_inserter(autocor),
-                                 subtract_mean,
-                                 /* output hierarchy? */ true);
+    const size_t N = ar::burg_method(istream_iterator<double>(cin),
+                                     istream_iterator<double>(),
+                                     mean,
+                                     order,
+                                     back_inserter(params),
+                                     back_inserter(sigma2e),
+                                     back_inserter(gain),
+                                     back_inserter(autocor),
+                                     subtract_mean,
+                                     /* output hierarchy? */ true);
 
-    // Keep only best model according to CIC accounting for subtract_mean.
-    if (subtract_mean) {
-        best_model<CIC<Burg<mean_subtracted> > >(
-                N, params, sigma2e, gain, autocor);
-    } else {
-        best_model<CIC<Burg<mean_retained> > >(
-                N, params, sigma2e, gain, autocor);
-    }
+
+    // Keep only best model according to selected criterion
+    best_model(N, params, sigma2e, gain, autocor);
 
     // Compute decorrelation time from the estimated autocorrelation model
-    const double T0  = decorrelation_time(N, autocorrelation(params.begin(),
-                params.end(), gain[0], autocor.begin()), absolute_rho);
+    const double T0  = ar::decorrelation_time(N, ar::autocorrelation(
+                params.begin(), params.end(), gain[0], autocor.begin()),
+                absolute_rho);
 
     // Output details about the best model and derived information
     // Unbiased effective variance expression from [Trenberth1984]
@@ -123,4 +155,33 @@ int main(int argc, char *argv[])
     cout.flush();
 
     return EXIT_SUCCESS;
+}
+
+// Argument checking logic suggested by optionparser.h example routines
+option::ArgStatus Arg::NonEmpty(const option::Option& opt, bool msg)
+{
+    if (opt.arg != 0 && opt.arg[0] != 0) {
+        return option::ARG_OK;
+    }
+
+    if (msg) {
+        (std::cerr << "Option ").write(opt.name, opt.namelen)
+                    << " requires a non-empty argument\n";
+    }
+    return option::ARG_ILLEGAL;
+}
+
+// Argument checking logic suggested by optionparser.h example routines
+option::ArgStatus Arg::Numeric(const option::Option& opt, bool msg)
+{
+    char *endptr;
+    if (opt.arg != 0 && strtol(opt.arg, &endptr, 10) && *endptr == 0) {
+        return option::ARG_OK;
+    }
+
+    if (msg) {
+        (std::cerr << "Option ").write(opt.name, opt.namelen)
+                    << " requires a numeric argument\n";
+    }
+    return option::ARG_ILLEGAL;
 }
